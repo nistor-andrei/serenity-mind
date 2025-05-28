3aad68981bb14f1e2186fa0f2b586b4086eacd55
nistor-andrei <andreimn97@yahoo.com>
2025-05-28 18:10:05 +0300

    fix: Update auth callback URL to use NEXT_PUBLIC_SITE_URL environment variable

--- utils/actions.ts
+++ utils/actions.ts
@@ -7,7 +7,7 @@ type Provider = "google" | "github" | "facebook";
 const signInWith = (provider: Provider) => async () => {
   const supabase = await createClient();
 
-  const auth_callback_url = `${process.env.SITE_URL}/auth/callback`;
+  const auth_callback_url = `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`;
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider,
     options: {
