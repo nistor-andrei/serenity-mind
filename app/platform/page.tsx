420dba578d92772b7cf1856afd5059139d96dbb4
nistor-andrei <andreimn97@yahoo.com>
2025-05-28 18:20:03 +0300

    Implement code changes to enhance functionality and improve performance

--- app/platform/page.tsx
+++ app/platform/page.tsx
@@ -1,4 +1,3 @@
-import { pipeline } from "@xenova/transformers";
 import { createClient } from "lib/supabaseServer";
 import { redirect } from "next/navigation";
 import JournalEntry from "./JournalEntry/JournalEntry";
@@ -16,38 +15,8 @@ async function getUser() {
   return data;
 }
 
-async function getMindfulnessTip() {
-  try {
-    const generator = await pipeline("text-generation", "Xenova/gpt2");
-    const prompt = "Mindfulness tip of the day:\n-";
-
-    const output = await generator(prompt, {
-      max_new_tokens: 30,
-      temperature: 0.7,
-      top_k: 40,
-      repetition_penalty: 1.2,
-    });
-
-    const generatedText =
-      Array.isArray(output) && output[0] && "generated_text" in output[0]
-        ? (output[0] as { generated_text: string }).generated_text
-        : typeof output === "object" &&
-          output !== null &&
-          "generated_text" in output
-        ? (output as { generated_text: string }).generated_text
-        : "";
-
-    return generatedText.replace(prompt, "").trim();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 export default async function PlatformPage() {
   const user = await getUser();
-  const tip = await getMindfulnessTip();
-
-  console.log(tip);
 
   return (
     <section className="grid grid-cols-1 xl:grid-cols-3 gap-6">
@@ -61,7 +30,7 @@ export default async function PlatformPage() {
         <WeeklyOverview />
       </div>
       <div className="xl:col-span-1">
-        <MindfulnessTip tip={tip} />
+        <MindfulnessTip />
       </div>
       <div className="xl:col-span-2">
         <JournalEntry />
